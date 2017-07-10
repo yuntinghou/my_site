9b397cab8888d946232470b03e9aa96d55be756e
yuntinghou <yunting.leo@gmail.com>
2017-07-10 02:14:15 -0700

    Unfinishied

---
+++ app/main/views.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from flask import render_template, session, redirect, url_for, current_app
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+from ..email import send_email
+
+@main.route('/')
+def index():
+    return render_template('future.html', current_time=datetime.utcnow())
+
+@main.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.contact'))
+    return render_template('contact.html', current_time=datetime.utcnow(), form=form, name=session.get('name'),
+                           known=session.get('known', False))
